77673eb3753707acbc342774c230bc79ccdbb44e
Benjamin Xia <benjx32@gmail.com>
2024-03-20 17:45:11 -0700

    Training loss

--- nn/ResNet.py
+++ nn/ResNet.py
@@ -1,25 +1,8 @@
+from typing import Tuple, Any
+
 import torch
 from torch import nn
 import torch.nn.functional as F
-
-
-class ResidualBlock(nn.Module):
-    def __init__(self, channel, kernel_size, stride=1, padding=None):
-        super(ResidualBlock, self).__init__()
-        if padding is None:
-            padding = 1
-        self.conv1 = nn.Conv2d(channel, channel, kernel_size=kernel_size, stride=stride, padding=padding)
-        self.conv2 = nn.Conv2d(channel, channel, kernel_size=kernel_size, stride=stride, padding=padding)
-        self.ln1 = nn.InstanceNorm2d(channel, affine=True)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        logits = self.conv1(x)
-        logits = F.relu(logits)
-        logits = self.conv2(logits)
-        logits = self.ln1(logits)
-        logits = logits + x
-        logits = F.relu(logits)
-        return logits
 
 
 class ResidualBlock(nn.Module):
@@ -52,8 +35,10 @@ class Encoder(nn.Module):
         self.conv2 = nn.ParameterList([nn.Conv2d(latent_channels, latent_channels, 3) for _ in range(20)])
         # to latent dim (20, 11, 79)
         self.conv3 = nn.Conv2d(latent_channels, latent_channels, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(10 * 6 * 278, 2 * latent_dim)
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        self.fc_mu = nn.Linear(10 * 6 * 278, latent_dim)
+        self.fc_logvar = nn.Linear(10 * 6 * 278, latent_dim)
+
+    def forward(self, x: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
         logit = self.conv0(x)
         logit = F.relu(logit)
 
@@ -72,8 +57,8 @@ class Encoder(nn.Module):
         logit = self.conv3(logit)
         logit = F.relu(logit)
 
-        logit = self.fc1(logit.reshape(logit.size()[0], -1))
-        return logit
+        logit = logit.reshape(logit.size()[0], -1)
+        return self.fc_mu(logit), self.fc_logvar(logit)
 
 
 class Decoder(nn.Module):
@@ -107,11 +92,30 @@ class Decoder(nn.Module):
         logit = self.conv0(logit)
         return logit
 
+
 class AudioVAE(nn.Module):
-    def __init__(self, latent_channels=10, input_size=(2, 128, 400)):
+    def __init__(self, latent_channels=10, latent_dim=100, input_size=(2, 128, 400)):
         super(AudioVAE, self).__init__()
         self.input_size = input_size
+        self.latent_channels = latent_channels
+        self.latent_dim = latent_dim
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+
+    def sample(self, mu: torch.Tensor, log_var: torch.Tensor):
+        eps = torch.randn_like(mu)
+        return eps.mul(torch.exp(0.5 * log_var)) + mu
+
+    def forward(self, x: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        mu, log_var = self.encoder(x)
+        z = self.sample(mu, log_var)
+        return self.decoder(z), mu, log_var
 
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        pass
+def ELBO_loss(recon_x, x, mu, logvar):
+    """
+    Evidence Lower Bound Loss
+    """
+    MSE = F.mse_loss(recon_x, x, reduction='sum')
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    return MSE + KLD
